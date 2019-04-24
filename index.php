c1ebdb46bf1509a4f3f86ec555a98ee5ad6beb1e
Oscar López <Oscar@Oscar.local>
2019-04-24 18:34:59 -0500

    JQuery 3.4.0 | Menú smooth fied top

--- index.php
+++ index.php
@@ -16,13 +16,49 @@
 
     <title>Laura y Roberto</title>
   </head>
-  <body>
+  <body class="animated fadeIn" id="section_1">
     
     <div id="wrapper">
 
-      <header id="header">
-        
+      <header id="header" class="fixed-top">
+        <div class="container">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">Laura y Roberto</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a href="#section_1" class="nav-link">Home <span>Conócenos</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#section_2" class="nav-link">Talleres <span>¿Quiénes somos?</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#section_3" class="nav-link">Conferencias <span>¿Quiénes somos?</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#section_4" class="nav-link">Asesorías <span>¿Quiénes somos?</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#section_5" class="nav-link">Servicios <span>¿Quiénes somos?</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#section_6" class="nav-link">Tu Tienda <span>¿Quiénes somos?</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="#section_7" class="nav-link">Contacto <span>¡Llámanos ahora!</span></a>
+                </li>
+              </ul>
+            </div><!--.collapse-->
+          </nav>
+        </div><!--.container-->
       </header>
+
+      <section id="slider">
+        
+      </section>
 
       <section id="section-welcome">
         <div class="container">
@@ -43,121 +79,130 @@
           
           <div class="container clearfix">
 
-            <h1 class="text-center title-section">Talleres</h1>
+            <section id="section_2">
+              <h1 class="text-center title-section">Talleres</h1>
+              <div class="row">
+                <div class="col-md-6">
+                  <h4>TALLER DE IMAGEN PERSONAL</h4>
+                  <p>¡Proyecta al mundo quién eres y qué haces!</p>
+                  <p>Cuando te preguntas “¿cómo me veo?” se debe a que muchas veces no sabes lo que te queda bien o si te ves adecuado a la actividad que vas a realizar.</p>
+                  <p>Aquí te ayudaremos a comunicar eficientemente a través de tus prendas y acorde a tus audiencias y objetivos, fortaleciendo al máximo tus virtudes.</p>
+                </div><!--.col-md-6-->
+                <div class="col-md-6">
+                  <h4>TALLER DE IMAGEN VERBAL Y LENGUAJE CORPORAL</h4>
+                  <p>¡No sólo hables, exprésate!</p>
+                  <p>A veces creemos que las palabras son suficientes para comunicarnos y lograr que los otros nos entiendan, sin embargo, se requiere tener un mensaje bien estructurado y que tu cuerpo lo acompañe con los gestos, ademanes y posturas correctos para generar el sentimiento que deseas transmitir.</p>
+                </div><!--.col-md-6-->
+                <div class="col-md-6">
+                  <h4>TALLER DE IMAGEN PROFESIONAL</h4>
+                  <p>¡Ampliar tus posibilidades de éxito está en tus manos!</p>
+                  <p>Vivimos en un mundo cada vez más competitivo, por ello no basta con ser excelente en lo que haces, también debes saber proyectarlo correctamente, mejorando tus relaciones dentro y fuera del trabajo.</p>
+                </div><!--.col-md-6-->
+                <div class="col-md-6">
+                  <h4>TALLER “ENCUENTRA CHAMBA”</h4>
+                  <p>¡Refleja tu conocimiento y experiencia!</p>
+                  <p>Muéstrale a tu reclutador lo mejor de ti, lanzando los estímulos clave que te destaquen entre tu competencia y te abran las puertas a ese nuevo empleo.</p>
+                </div><!--.col-md-6-->
+              </div><!--.row-->
+            </section>
 
-            <div class="row">
-              <div class="col-md-6">
-                <h4>TALLER DE IMAGEN PERSONAL</h4>
-                <p>¡Proyecta al mundo quién eres y qué haces!</p>
-                <p>Cuando te preguntas “¿cómo me veo?” se debe a que muchas veces no sabes lo que te queda bien o si te ves adecuado a la actividad que vas a realizar.</p>
-                <p>Aquí te ayudaremos a comunicar eficientemente a través de tus prendas y acorde a tus audiencias y objetivos, fortaleciendo al máximo tus virtudes.</p>
-              </div><!--.col-md-6-->
-              <div class="col-md-6">
-                <h4>TALLER DE IMAGEN VERBAL Y LENGUAJE CORPORAL</h4>
-                <p>¡No sólo hables, exprésate!</p>
-                <p>A veces creemos que las palabras son suficientes para comunicarnos y lograr que los otros nos entiendan, sin embargo, se requiere tener un mensaje bien estructurado y que tu cuerpo lo acompañe con los gestos, ademanes y posturas correctos para generar el sentimiento que deseas transmitir.</p>
-              </div><!--.col-md-6-->
-              <div class="col-md-6">
-                <h4>TALLER DE IMAGEN PROFESIONAL</h4>
-                <p>¡Ampliar tus posibilidades de éxito está en tus manos!</p>
-                <p>Vivimos en un mundo cada vez más competitivo, por ello no basta con ser excelente en lo que haces, también debes saber proyectarlo correctamente, mejorando tus relaciones dentro y fuera del trabajo.</p>
-              </div><!--.col-md-6-->
-              <div class="col-md-6">
-                <h4>TALLER “ENCUENTRA CHAMBA”</h4>
-                <p>¡Refleja tu conocimiento y experiencia!</p>
-                <p>Muéstrale a tu reclutador lo mejor de ti, lanzando los estímulos clave que te destaquen entre tu competencia y te abran las puertas a ese nuevo empleo.</p>
-              </div><!--.col-md-6-->
-            </div><!--.row-->
+            <section id="section_3">
+              <h1 class="text-center title-section">Conferencias</h1>
+              <h4>TU IMAGEN HABLA BIEN DE TI</h4>
+              <p>Buscamos que quienes asistan, analicen y comprendan el impacto y los beneficios del cuidado de la imagen en el ámbito personal y profesional.</p>
+              <p>Para ello, evaluamos los hábitos positivos y negativos que a lo largo de la vida todos hemos adquirido, ejemplificando de forma simple y útil cuáles son los que debemos conservar, cuáles incorporar y cuáles desechar.</p>
+            </section>
 
-            <h1 class="text-center title-section">Conferencias</h1>
+            <section id="section_4">
+              <h1 class="text-center title-section">Asesorías</h1>
+              <h4>ASESORÍA EN IMAGEN PERSONAL</h4>
+              <p>Renueva tu imagen, cambia tu vida.</p>
+              <p>Si deseas darle un refresh a tu imagen, nosotros te apoyamos. Con base en tu esencia, tus necesidades y las de tus audiencias, y a partir de las formas y medidas de tu cara y cuerpo, te diremos qué tipo de prendas y accesorios son los más adecuados para verte como siempre quisiste y alcanzar tus objetivos.</p>
 
-            <h4>TU IMAGEN HABLA BIEN DE TI</h4>
-            <p>Buscamos que quienes asistan, analicen y comprendan el impacto y los beneficios del cuidado de la imagen en el ámbito personal y profesional.</p>
-            <p>Para ello, evaluamos los hábitos positivos y negativos que a lo largo de la vida todos hemos adquirido, ejemplificando de forma simple y útil cuáles son los que debemos conservar, cuáles incorporar y cuáles desechar.</p>
+              <h4>ASESORÍA EN IMAGEN VERBAL Y NO VERBAL</h4>
+              <p>Comunica lo que quieres de forma adecuada, eficiente y atractiva.</p>
+              <p>¿Tienes miedo de hablar en público?, ¿te cuesta decir lo que piensas?, ¿sientes que la gente no te entiende? Ven con nosotros y aprende qué comunica tu cuerpo y cómo debes estructurar mensajes claros y precisos que te ayuden a comunicar lo que quieres y a generar la emoción que deseas.</p>
 
-            <h1 class="text-center title-section">Asesorías</h1>
+              <h4>ASESORÍA EN IMAGEN PROFESIONAL</h4>
+              <p>Para ser, hay que parecer.</p>
+              <p>Si estás decidido a crecer profesionalmente, esta asesoría es para ti. Aprende a generar, a través de tu ropa, tus palabras, tu cuerpo y tu trato, los estímulos necesarios que acompañen tus conocimientos, capacidades y experiencia para alcanzar el puesto que anhelas.</p>
+            </section>
 
-            <h4>ASESORÍA EN IMAGEN PERSONAL</h4>
-            <p>Renueva tu imagen, cambia tu vida.</p>
-            <p>Si deseas darle un refresh a tu imagen, nosotros te apoyamos. Con base en tu esencia, tus necesidades y las de tus audiencias, y a partir de las formas y medidas de tu cara y cuerpo, te diremos qué tipo de prendas y accesorios son los más adecuados para verte como siempre quisiste y alcanzar tus objetivos.</p>
+            <section id="section_5">
+              <h1 class="text-center title-section">Servicios</h1>
+            </section>
 
-            <h4>ASESORÍA EN IMAGEN VERBAL Y NO VERBAL</h4>
-            <p>Comunica lo que quieres de forma adecuada, eficiente y atractiva.</p>
-            <p>¿Tienes miedo de hablar en público?, ¿te cuesta decir lo que piensas?, ¿sientes que la gente no te entiende? Ven con nosotros y aprende qué comunica tu cuerpo y cómo debes estructurar mensajes claros y precisos que te ayuden a comunicar lo que quieres y a generar la emoción que deseas.</p>
+            <section id="section_6">
+              <h1 class="text-center title-section">Tu Tienda</h1>
+              <div class="row">
+                <div class="col-sm-6">
+                  <p>Proactivity Shopper</p>
+                  <p>¡Evoluciona, agrega valor y vende!</p>
+                  <p>Llévanos a tu tienda y te ayudaremos a consentir a tus clientes y agregar valor a tu oferta. A través de un sencillo análisis, les diremos a tus clientes qué prendas les van mejor de acuerdo con sus características físicas, lo que sin duda potenciará el deseo de compra y promoverá la lealtad hacia tu marca.</p>
+                </div><!--.col-sm-6-->
+                <div class="col-sm-6">
+                  <p>Diseño de escaparate</p>
+                  <p>¡El amor sí entra por los ojos!</p>
+                  <p>Analizando tus productos, tu marca y el mercado al que vas dirigido, haremos que tu escaparate sea sumamente atractivo y provoque el deseo de compra mediante las prendas y accesorios que ofreces.</p>
+                </div><!--.col-sm-6-->
+              </div><!--.row-->
+            </section>
 
-            <h4>ASESORÍA EN IMAGEN PROFESIONAL</h4>
-            <p>Para ser, hay que parecer.</p>
-            <p>Si estás decidido a crecer profesionalmente, esta asesoría es para ti. Aprende a generar, a través de tu ropa, tus palabras, tu cuerpo y tu trato, los estímulos necesarios que acompañen tus conocimientos, capacidades y experiencia para alcanzar el puesto que anhelas.</p>
-
-            <h1 class="text-center title-section">Tu Tienda</h1>
-
-            <div class="row">
-              <div class="col-sm-6">
-                <p>Proactivity Shopper</p>
-                <p>¡Evoluciona, agrega valor y vende!</p>
-                <p>Llévanos a tu tienda y te ayudaremos a consentir a tus clientes y agregar valor a tu oferta. A través de un sencillo análisis, les diremos a tus clientes qué prendas les van mejor de acuerdo con sus características físicas, lo que sin duda potenciará el deseo de compra y promoverá la lealtad hacia tu marca.</p>
-              </div><!--.col-sm-6-->
-              <div class="col-sm-6">
-                <p>Diseño de escaparate</p>
-                <p>¡El amor sí entra por los ojos!</p>
-                <p>Analizando tus productos, tu marca y el mercado al que vas dirigido, haremos que tu escaparate sea sumamente atractivo y provoque el deseo de compra mediante las prendas y accesorios que ofreces.</p>
-              </div><!--.col-sm-6-->
-            </div><!--.row-->
-
-            <h1 class="text-center title-section">Contacto</h1>
-
-            <div class="row">
-              <div class="col-sm-6">
-                <form method="POST">
-                  <div class="row">
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label for="name">Nombre</label>
-                        <input type="text" class="form-control form-control-lg" id="name" name="name">
-                      </div><!--.form-group-->
-                    </div><!--.col-md-4-->
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control form-control-lg" id="email" name="email">
-                      </div><!--.form-group-->
-                    </div><!--.col-md-4-->
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label for="phone">Teléfono</label>
-                        <input type="text" class="form-control form-control-lg" id="phone" name="phone">
-                      </div><!--.form-group-->
-                    </div><!--.col-md-4-->
-                    <div class="col-md-8">
-                      <div class="form-group">
-                        <label for="subject">Asunto</label>
-                        <input type="text" class="form-control form-control-lg" id="subject" name="subject">
-                      </div><!--.form-group-->
-                    </div><!--.col-md-8-->
-                    <div class="col-md-4">
-                      <div class="form-group">
-                        <label for="name">Servicio</label>
-                        <select class="custom-select custom-select-lg mb-3">
-                          <option selected>Open this select menu</option>
-                          <option value="1">One</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                        </select>
-                      </div><!--.form-group-->
-                    </div><!--.col-md-4-->
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label for="message">Asunto</label>
-                        <textarea class="form-control" id="message" name="message" rows="3"></textarea>
-                      </div><!--.form-group-->
-                    </div><!--.col-md-12-->
-                  </div><!--.row-->
-                </form>
-              </div><!--.col-sm-6-->
-              <div class="col-sm-6">
-                
-              </div><!--.col-sm-6-->
-            </div><!--.row-->
+            <section id="section_7">
+              <h1 class="text-center title-section">Contacto</h1>
+              <div class="row">
+                <div class="col-sm-6">
+                  <form method="POST">
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="name">Nombre</label>
+                          <input type="text" class="form-control form-control-lg" id="name" name="name">
+                        </div><!--.form-group-->
+                      </div><!--.col-md-4-->
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="email">Email</label>
+                          <input type="email" class="form-control form-control-lg" id="email" name="email">
+                        </div><!--.form-group-->
+                      </div><!--.col-md-4-->
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="phone">Teléfono</label>
+                          <input type="text" class="form-control form-control-lg" id="phone" name="phone">
+                        </div><!--.form-group-->
+                      </div><!--.col-md-4-->
+                      <div class="col-md-8">
+                        <div class="form-group">
+                          <label for="subject">Asunto</label>
+                          <input type="text" class="form-control form-control-lg" id="subject" name="subject">
+                        </div><!--.form-group-->
+                      </div><!--.col-md-8-->
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="name">Servicio</label>
+                          <select class="custom-select custom-select-lg mb-3">
+                            <option selected>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                        </div><!--.form-group-->
+                      </div><!--.col-md-4-->
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label for="message">Asunto</label>
+                          <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+                        </div><!--.form-group-->
+                      </div><!--.col-md-12-->
+                    </div><!--.row-->
+                  </form>
+                </div><!--.col-sm-6-->
+                <div class="col-sm-6">
+                  
+                </div><!--.col-sm-6-->
+              </div><!--.row-->
+            </section>
             
           </div><!--.container-->
 
@@ -172,7 +217,7 @@
     </div><!--#wrapper-->
 
     <!-- jQuery, Popper.js and Bootstrap JS -->
-    <script type="text/javascript" src="./js/jquery-3.3.1.slim.min.js"></script>
+    <script type="text/javascript" src="./js/jquery-3.4.0.min.js"></script>
     <script type="text/javascript" src="./js/popper.min.js"></script>
     <script type="text/javascript" src="./js/bootstrap.min.js"></script>
 
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
 
+	var sectionArray = [1, 2, 3, 4, 5, 6, 7];
+	$.each(sectionArray, function(index, value){
+	     $(document).scroll(function(){
+	         var offsetSection = $('#' + 'section_' + value).offset().top;
+	         var docScroll = $(document).scrollTop();
+	         var docScroll1 = docScroll + 1; 
+	         if ( docScroll1 >= offsetSection ){
+	             $('li').removeClass('active');
+	             $('li:link').addClass('inactive');  
+	             $('li').eq(index).addClass('active');
+	             $('li:link').eq(index).removeClass('inactive');
+	         }
+	     });
+	    $('li a').eq(index).click(function(e){
+	        var offsetClick = $('#' + 'section_' + value).offset().top;
+	        e.preventDefault();
+	        $('html, body').animate({
+	            'scrollTop':offsetClick
+	        }, 300);
+	    });
+	});
+
+	    // $('.nav-item:link').addClass('inactive');    
+	    // $('.nav-item').eq(0).addClass('active');
+	    // $('.nav-item:link').eq(0).removeClass('inactive');
+	
+
 	$(window).on("scroll", function() {
 	  if($(window).scrollTop() > 40) {
-	    $("#header").addClass("navbar-fixed");
+	    $("#header").addClass("shadow-sm");
 	  } else {
-	    $("#header").removeClass("navbar-fixed");
+	    $("#header").removeClass("shadow-sm");
 	  }
 	});
 
